21eb8eb0839dd72db391e83ce0d539c82fb95bdf
Matthew Wall <mwall@picorino.local>
2017-01-06 22:05:07 -0500

    initial import

---
+++ install.py
@@ -0,0 +1,18 @@
+# installer for the weewx-observer driver
+# Copyright 2017 Matthew Wall, all rights reserved
+
+from setup import ExtensionInstaller
+
+def loader():
+    return ObserverInstaller()
+
+class ObserverInstaller(ExtensionInstaller):
+    def __init__(self):
+        super(ObserverInstaller, self).__init__(
+            version="0.1",
+            name='observer',
+            description='Capture data from observer weather station',
+            author="Matthew Wall",
+            author_email="mwall@users.sourceforge.net",
+            files=[('bin/user', ['bin/user/observer.py'])]
+            )
---
+++ readme.txt
@@ -0,0 +1,24 @@
+weewx-observer
+
+This is a driver for weewx that captures data by polling an observer weather
+station over wifi.
+
+
+===============================================================================
+Installation
+
+1) download the driver
+
+wget -O weewx-observer.zip https://github.com/matthewwall/weewx-observer/archive/master.zip
+
+2) install the driver
+
+sudo wee_extension --install weewx-observer.zip
+
+3) configure the driver
+
+sudo wee_config --reconfigure driver=user.observer --no-prompt
+
+4) start weewx
+
+sudo /etc/init.d/weewx start
